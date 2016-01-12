d834745cfdda06eed94939ab65c4cbca33be1b40
Tiancheng Shen <tshen@cs.ualberta.ca>
2016-01-12 09:27:05 -0700

    create server

---
+++ clientdemo.py
@@ -0,0 +1,20 @@
+#!/user/bin/env python
+
+import socket
+
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+clientSocket.connect(("www.google.com",80))
+
+request = "GET / HTTP/1.0\n\n"
+
+clientSocket.sendall(request)
+
+done = False
+
+while not done:
+    part = clientSocket.recv(2048)
+    if (part):
+        print part
+    else:
+        done = True
